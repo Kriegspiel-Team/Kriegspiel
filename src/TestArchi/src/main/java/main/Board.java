1fee7e54f8ca8f0f92127affbb1709aad4da147b
reywas <davidcheminade@hotmail.fr>
2014-03-28 18:52:50 +0100

    Debut de rapport

--- src/TestArchi/src/main/java/main/Board.java
+++ src/TestArchi/src/main/java/main/Board.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-import model.Arsenal;
 import model.Cavalry;
 import model.Entity;
 import model.Fortress;
@@ -37,13 +36,6 @@ public class Board {
 	
 	public void loadBoardWithFile(String filename) {
 		new EntityLoader(this, filename);
-	}
-	
-	public void resetBoard(){
-		matrix = new Entity[WIDTH][HEIGHT];
-		coord_arsenals.clear();
-		communications.get(0).clear();
-		communications.get(1).clear();
 	}
 	
 	public void saveArsenalPlacement(int x, int y) {
@@ -111,10 +103,6 @@ public class Board {
 	
 	public boolean isMountain(int x, int y) {
 		return matrix[x][y] instanceof Mountain;
-	}
-	
-	public boolean isArsenal(int x, int y) {
-		return matrix[x][y] instanceof Arsenal;
 	}
 	
 	public boolean isMountainPass(int x, int y) {
@@ -204,7 +192,9 @@ public class Board {
 	}
 	
 	public void computeCommunications(int x, int y, int team) {
-				
+		
+		System.out.println(team);
+		
 		int i = 0;
 		boolean north = true, south = true, east = true, west = true, northest = true, northwest = true, southeast = true, southwest = true;
 				
